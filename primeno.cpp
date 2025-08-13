5a2be6f08ef06081175e7c4482e885d6345027a6
Tusshar <tushargawhade7@gmail.com>
2025-08-13 20:15:16 +0530

    Prime number program

---
+++ primeno.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=10;
+    bool isprime =true;
+    for(int i=2;i<n-1;i++){
+        if(n%i==0){
+            isprime = false;
+            break;
+        }
+    }
+
+    if(isprime==true){
+        cout<<"Number is a prime number."<<endl;
+    }
+    else{
+        cout<<"Number is not prime."<<endl;
+    }
+
+    return 0;
+}
