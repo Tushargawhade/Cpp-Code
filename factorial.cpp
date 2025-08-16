b89a244ff76c54d9697ee71727c54a50cef214a3
Tusshar <tushargawhade7@gmail.com>
2025-08-16 17:53:19 +0530

    factorial program

---
+++ factorial.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int factorial(int num){
+    if(num==0 || num==1){
+        return 1;
+    }
+    else{
+        return  num * factorial(num-1);
+    }
+}
+int main(){
+    int num;
+
+    cout<<"Enter a number : ";
+    cin>>num;
+
+    cout<<"Factorial of "<<num<<" is : "<<factorial(num);
+}
