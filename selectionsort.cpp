f2967ffadff40ad87b3d5f0a89a0ddcfc5b0f064
Tusshar <tushargawhade7@gmail.com>
2025-08-17 23:16:59 +0530

    selection sort

---
+++ selectionsort.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+void printarr(int arr[],int n){
+   for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+   }
+
+}
+void selectionsort(int arr[],int n){
+    for(int i=0;i<n-1;i++){
+        int minidx =i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[minidx]){
+                minidx =j;
+            }
+        }
+        swap(arr[i],arr[minidx]);
+
+    }
+    printarr(arr,n);
+}
+int main(){
+    int arr[]={5,4,1,3,2};
+    int n=sizeof(arr)/sizeof(int);
+    selectionsort(arr,n);
+
+    return 0;
+
+
+
+}
