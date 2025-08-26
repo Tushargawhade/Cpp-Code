b5d1139414895b01c0579ae2d6dd3c252195cb53
Tusshar <tushargawhade7@gmail.com>
2025-08-26 20:12:33 +0530

    Extreme number program.

--- ExtremPrint.cpp
+++ ExtremPrint.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
 
-    int arr[10]={10,20,30,40,50,60,70,80};
+    int arr[8]={10,20,30,40,50,60,70,80};
     int size = 8;
     int start = 0;
     int end = size-1;
