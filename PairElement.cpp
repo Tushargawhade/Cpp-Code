07e708c36d6a45f64c4c52144e70c5c63f685957
Tusshar <tushargawhade7@gmail.com>
2025-08-29 18:17:53 +0530

    Pair sum problem

--- PairElement.cpp
+++ PairElement.cpp
@@ -5,7 +5,7 @@ int main(){
 
     vector<int> arr{10,20,30,40,50};
     int sum = 60;
-    cout<<"The pair of sum "<<sum<<" is :- "<<endl;
+    cout<<"The pair of "<<sum<<" is :- "<<endl;
     for(int i=0; i<arr.size(); i++){
         for(int j=i+1; j<arr.size(); j++){
             if(arr[i] + arr[j] == sum){
